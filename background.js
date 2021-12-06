ecf47e3e50685f985a3b2812f1342f10e7b34d62
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2021-12-06 19:18:46 +0900

    Revert "Improved smartphone support and compatibility."
    This reverts commit 4318b54d2dde92706cfa08862addefa5ad2c4833.

--- background.js
+++ background.js
@@ -1,19 +1,3 @@
-var targetPage = "https://lens.google.com/*";
-var ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
-function rewriteUserAgentHeader(e) {
-    e.requestHeaders.forEach(function(header){
-        if (header.name.toLowerCase() === "user-agent") {
-            header.value = ua;
-        }
-    });
-    return {requestHeaders: e.requestHeaders};
-}
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    rewriteUserAgentHeader,
-    {urls: [targetPage]},
-    ["blocking", "requestHeaders"]
-);
-
 function generateRandomByteString(n){
     var str = "";
     for (var i = 0; i < n; i++){

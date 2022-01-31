ae4902e26e8a2df6bd6750626fe3d5b16b2ef922
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-01-31 17:23:45 +0900

    Enhanced privacy and made the code easier to read.

--- background.js
+++ background.js
@@ -1,84 +1,71 @@
 var targetPage = "https://lens.google.com/*";
 var ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4725.0 Safari/537.36";
 function rewriteUserAgentHeader(e) {
-    e.requestHeaders.forEach(function(header){
+    e.requestHeaders.forEach(function (header) {
         if (header.name.toLowerCase() === "user-agent") {
             header.value = ua;
         }
     });
-    return {requestHeaders: e.requestHeaders};
+    return { requestHeaders: e.requestHeaders };
 }
 chrome.webRequest.onBeforeSendHeaders.addListener(
     rewriteUserAgentHeader,
-    {urls: [targetPage]},
+    { urls: [targetPage] },
     ["blocking", "requestHeaders"]
 );
 
-function generateRandomByteString(n){
+function generateRandomString(n) {
+    var s = "abcdefghijklmnopqrstuvwxyz0123456789";
     var str = "";
-    for (var i = 0; i < n; i++){
-        str += Math.floor(Math.random() * 16).toString(16);
+    for (var i = 0; i < n; i++) {
+        str += s[Math.floor(Math.random() * s.length)];
     }
     return str;
 }
 
 function search_on_google_lens(image_url, tab) {
-    chrome.tabs.sendMessage(tab.id, "load-start")
-
-    //fetchでやったほうがいいのはわかってるけど、使い方がいまいちわからんので、とりあえずXHR。理解したら、書き換える。
-
-    function imaeg_get_error() {
-        chrome.tabs.sendMessage(tab.id, "image-get-error")
-    }
-
-    function google_post_error() {
-        chrome.tabs.sendMessage(tab.id, "google-post-error")
-    }
-
-    function reqListener() {
-        if (this.status != 200) {
-            imaeg_get_error();
-            return;
-        }
-        chrome.tabs.sendMessage(tab.id, "image-get-end")
-
-        var res = this.response;
-        console.log(res);
-
-        var form = new FormData()
-        form.set("encoded_image", res)
-        form.set("image_url", "https://" + generateRandomByteString(12) + ".com/" + generateRandomByteString(12));//プライバシー保護のため、適当なURLを送信
-        form.set("sbisrc", "Chromium 98.0.4725.0 Windows")
-
-        function reqListener2() {
-            if (this.status != 200) {
-                google_post_error();
-                return;
+    chrome.tabs.sendMessage(tab.id, "load-start");
+    fetch(image_url)
+        .then(res => {
+            if (res.status === 200) {
+                return res.blob();
+            }else{
+                chrome.tabs.sendMessage(tab.id, "image-get-error");
             }
-
-            chrome.tabs.sendMessage(tab.id, "google-post-end")
-
-            var res = this.responseText;
-            console.log(res)
-            var url = res.match(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/)[0];
-            console.log(url)
-            chrome.tabs.create({ url: url })
-        }
-        var xhr2 = new XMLHttpRequest();
-        xhr2.open("POST", "https://lens.google.com/upload?ep=ccm&s=&st=" + (new Date()).getTime())
-        xhr2.addEventListener("load", reqListener2)
-        xhr2.addEventListener("error", google_post_error)
-        xhr2.send(form)
-    }
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", reqListener)
-    xhr.addEventListener("error", imaeg_get_error)
-    xhr.open("GET", image_url);
-    xhr.responseType = "blob"
-    xhr.send();
+        }).then(data => {
+            if (data) {
+                chrome.tabs.sendMessage(tab.id, "image-get-end");
+                var form = new FormData()
+                form.set("encoded_image", data)
+                form.set("image_url", "https://" + generateRandomString(12) + ".com/" + generateRandomString(12));//プライバシー保護のため、適当なURLを送信
+                form.set("sbisrc", "Chromium 98.0.4725.0 Windows")
+                fetch("https://lens.google.com/upload?ep=ccm&s=&st=" + generateRandomString(12), {
+                    method: "POST",
+                    body: form
+                }).then(res => {
+                    if (res.status === 200) {
+                        return res.text();
+                    }else{
+                        chrome.tabs.sendMessage(tab.id, "google-post-error");
+                    }
+                }).then(data => {
+                    if (data) {
+                        chrome.tabs.sendMessage(tab.id, "google-post-end");
+                        console.log(data)
+                        var url = data.match(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/)[0];
+                        console.log(url)
+                        chrome.tabs.create({ url: url })
+                    }
+                }).catch(error => {
+                    chrome.tabs.sendMessage(tab.id, "google-post-error");
+                })
+            }
+        }).catch(error => {
+            chrome.tabs.sendMessage(tab.id, "image-get-error");
+        })
 }
 
-chrome.browserAction.onClicked.addListener(function(){
+chrome.browserAction.onClicked.addListener(function () {
     chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") })
 })
 
@@ -97,6 +84,6 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
     }
 })
 
-chrome.runtime.onMessage.addListener(function(message, sender){
+chrome.runtime.onMessage.addListener(function (message, sender) {
     search_on_google_lens(message, sender.tab)
 })

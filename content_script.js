2649a065820fe0556b23a1efd9f7a27d69db776d
typeling1578 <pub@typeling1578.dev>
2023-10-02 02:57:36 +0900

    refactor

--- content_script.js
+++ content_script.js
@@ -2,10 +2,91 @@ if (!window.browser) {
     window.browser = chrome;
 }
 
-if(location.hostname == "lens.google.com"){
+if (location.hostname == "lens.google.com") {
     let sc = document.createElement("script");
     sc.src = browser.runtime.getURL("touch_start_event_blocking.js");
     document.documentElement.appendChild(sc);
+}
+
+if (location.protocol == "moz-extension:") {
+    const supported_file_types = [
+        "image/png",
+        "image/jpeg",
+        "image/webp",
+        "image/tiff",
+    ];
+
+    const button_elem = document.getElementById("button_elem");
+    const dropzone = document.getElementById("dropzone");
+    const fileinput = document.getElementById("fileinput");
+    const preview = document.getElementById("preview");
+
+    dropzone.addEventListener("dragover", function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.classList = ["file-dragover"];
+    });
+
+    dropzone.addEventListener("dragleave", function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.classList = ["file-dragleave"];
+    });
+
+    dropzone.addEventListener("drop", function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.classList = ["file-drop"];
+        const files = e.dataTransfer.files;
+        if (files.length > 1){
+            alert("Only one file can be uploaded at a time.");
+            return;
+        }
+        fileinput.files = files;
+        if (!supported_file_types.includes(files[0].type)) {
+            alert("Unsupported file type.");
+            return;
+        }
+        previewfile(files[0]);
+    });
+
+    fileinput.addEventListener("change", function () {
+        if (!supported_file_types.includes(files[0].type)) {
+            alert("Unsupported file type.");
+            return;
+        }
+        previewfile(this.files[0]);
+    });
+
+    function previewfile(file) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function() {
+            preview.innerHTML = "";
+            let img = document.createElement("img");
+            img.style.maxWidth = "100%";
+            img.style.maxHeight = "70vh";
+            img.style.border = "1px solid";
+            img.src = reader.result;
+            preview.appendChild(img);
+        };
+    }
+
+    button_elem.addEventListener("click",function() {
+        browser.runtime.sendMessage({ type: "send-image", src: preview.getElementsByTagName("img")[0].src });
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        let elems = document.getElementsByClassName("i18n-text");
+        for (let i = 0; i < elems.length; i++) {
+            elems[i].innerText = browser.i18n.getMessage(elems[i].getAttribute("data-i18n-id"));
+        }
+
+        let elems2 = document.getElementsByClassName("i18n-value");
+        for (let i = 0; i < elems2.length; i++) {
+            elems2[i].value = browser.i18n.getMessage(elems2[i].getAttribute("data-i18n-id"));
+        }
+    });
 }
 
 function inject_loading_element() {
@@ -32,7 +113,7 @@ function inject_loading_element() {
     var main_elem_img = document.createElement("img");
     main_elem_img.style.width = "60px";
     main_elem_img.style.height = "60px";
-    main_elem_img.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iMjAwcHgiIGhlaWdodD0iMjAwcHgiIHZpZXdCb3g9IjAgMCAxMDAgMTAwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJ4TWlkWU1pZCI+CjxjaXJjbGUgY3g9IjUwIiBjeT0iNTAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzQyODVmNCIgc3Ryb2tlLXdpZHRoPSIxMCIgcj0iMzUiIHN0cm9rZS1kYXNoYXJyYXk9IjE2NC45MzM2MTQzMTM0NjQxNSA1Ni45Nzc4NzE0Mzc4MjEzOCI+CiAgPGFuaW1hdGVUcmFuc2Zvcm0gYXR0cmlidXRlTmFtZT0idHJhbnNmb3JtIiB0eXBlPSJyb3RhdGUiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiBkdXI9IjFzIiB2YWx1ZXM9IjAgNTAgNTA7MzYwIDUwIDUwIiBrZXlUaW1lcz0iMDsxIj48L2FuaW1hdGVUcmFuc2Zvcm0+CjwvY2lyY2xlPgo8L3N2Zz4=";
+    main_elem_img.src = browser.runtime.getURL("loading.svg");
 
     var main_elem_text = document.createElement("div");
     main_elem_text.innerText = browser.i18n.getMessage("fetchingImage");
@@ -70,15 +151,15 @@ function google_post_error_elem_remove() {
 browser.runtime.onMessage.addListener(function (message) {
     switch (message.type) {
         case "load-start":
-            console.log("load-start")
+            console.log("load-start");
             inject_loading_element();
             break;
         case "image-get-end":
-            console.log("image-get-end")
+            console.log("image-get-end");
             image_get_end_elem_update();
             break;
         case "google-post-end":
-            console.log("google-post-end")
+            console.log("google-post-end");
             google_post_end_elem_remove();
             break;
         case "image-get-error":
